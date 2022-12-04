6c78fcd091a4612fd98aa83a965b5e67e3d5dbd5
Joe Divinagracia <SgtVarmint2112@protonmail.com>
2022-12-04 18:32:16 -0500

    Looked into turning normal functions into arrow functions to try and keep up to date with JS best practices; Simplifed username cleaning function; Refactored brackets to match Jons preferred style

--- components/homepage/js/home.js
+++ components/homepage/js/home.js
@@ -1,8 +1,27 @@
 const byID = (elementName) => document.getElementById(elementName);
+
 const getLocal = (itemName) => localStorage.getItem(itemName);
+
 const setLocal = (itemName, value) => localStorage.setItem(itemName, value);
 
-function init() {
+const checkAuthorization = (response) => 
+{
+    if(response.status === 'denied') 
+    {
+        byID('lobbyButton').onclick = () => 
+        {
+            toastMessage("You are not authenticated for access to this site", 2, "body", "5%", "13%", "13%", "0");
+        };
+    } 
+    else 
+    {
+        byID('lobbyButton').disabled = false;
+        setLocal('auth', response.status);
+    }
+};
+
+const init = () =>
+{
     byID('lobbyInput').value = getLocal('lobbyName');
     byID('userInput').value = getLocal('userName');
     byID('lobbyButton').onclick = lobbyButtonClick;
@@ -12,37 +31,28 @@ function init() {
     
     authenticate()
         .then((response) => { return response.json() })
-        .then((response) => {
-            if(response.status === 'denied') {
-                document.getElementById("lobbyButton").onclick = function(){ 
-                    toastMessage("You are not authenticated for access to this site", 2, "body", "5%", "13%", "13%", "0"); 
-                };
-            } else {
-                byID('lobbyButton').disabled = false;
-                setLocal('auth', response.status);
-            }
-        });
+        .then(checkAuthorization);
 }
 
-/**
- * 
- */
-function focusAuthInput() {
+const focusAuthInput = () => 
+{
     byID('auth').classList.toggle('hidden');
     byID('authText').focus();
 }
 
-async function authenticate(){
+async function authenticate()
+{
     let params = new FormData;
-    params.append('accessCode', localStorage.getItem('access'));
-    const response = await fetch('/components/homepage/php/authentication.php', {
+    params.append('accessCode', getLocal('access'));
+    const response = await fetch('/components/homepage/php/authentication.php', 
+    {
         method: 'POST',
         body: params
     });
     return response;
 }
 
-function authButtonClick()
+const authButtonClick = () => 
 {
     setLocal('access', byID('authText').value);
 	setLocal('lobbyName', byID("lobbyInput").value.toUpperCase());
@@ -50,8 +60,33 @@ function authButtonClick()
 	location.reload();
 }
 
-function lobbyButtonClick() {
+const lobbyButtonClick = () => 
+{
+    let lobbyName = byID('lobbyInput').value.toUpperCase();
+    let userName = formatUsername(byID('userInput').value);
 
+    setLocal('lobbyName', lobbyName);
+    setLocal('userName', userName);
+
+    if(!getLocal('userId')) 
+    {
+
+    }
+}
+
+/**
+ * Cleans up username provided by user input by stripping bad characters
+ * @param {*} username 
+ * @returns 
+ */
+const formatUsername = (username) => 
+{	
+	return username.replaceAll(/<|>/g, "").substring(0, 18);
+}
+
+const redirect = () => 
+{
+    
 }
 
 window.addEventListener('load', init);
