1f4a494741ef7470cd82f04501da096f2399fe09
Ryan Ehmann <RyanKEhmann@gmail.com>
2022-12-12 22:55:00 -0500

    Will still need updated
    but should be good enough for now
    
    - change hero image to something else, and colors

---
+++ Scripts/AuthSlider.js
@@ -0,0 +1,9 @@
+//make this the auth slide thing up and down from the hero image
+window.onload = function () {
+    const auth_menu = document.querySelector('#authMenu')
+    const auth_btn = document.querySelector('#authMenuButton')
+    auth_btn.addEventListener('click', function () {
+        auth_menu.classList.toggle('hidden')
+        auth_menu.classList.toggle('vertSlider')
+    })
+}
