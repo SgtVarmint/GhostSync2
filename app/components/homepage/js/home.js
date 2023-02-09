fe317fecef41d4add9c936769ed58f1bc753b3fa
Joe Divinagracia <SgtVarmint2112@protonmail.com>
2023-02-09 15:26:00 -0500

    Added keyEvents to the applicable elements and added some null value protection when populating textfields

--- app/components/homepage/js/home.js
+++ app/components/homepage/js/home.js
@@ -1,48 +1,69 @@
 const init = () =>
 {
-    byID('lobbyInput').value = getLocal('lobbyName');
-    byID('userInput').value = getLocal('userName');
-    
+    byID('lobbyInput').value = getCookie('lobbyName') ?? '';
+    byID('userInput').value = getCookie('userName') ?? '';
+    byID('authText').value = getCookie('accessKey') ?? '';
+
+    setKeyEvents();
+
     byID('authMenuButton').onclick = toggleAuthField;
     byID('authButton').onclick = authButtonClick;
+
+    byID('themeChange').onclick = changeTheme;
 }
 
-const byID = (elementName) => document.getElementById(elementName);
+const setKeyEvents = () => {
+    window.addEventListener('keyup', (event) => {
+        if(event.code === 'Enter') {
+            event.preventDefault();
+			document.getElementById("userInput").focus(); 
+        }
+    });
 
-const getLocal = (itemName) => localStorage.getItem(itemName);
-
-const setLocal = (itemName, value) => localStorage.setItem(itemName, value);
+    document.getElementById("userInput").addEventListener("keyup", (event) => {
+		if (event.code === 'Enter')
+		{
+			event.preventDefault();
+			document.getElementById("lobbyButton").click();
+		}
+	});
+	
+	document.getElementById("authText").addEventListener("keyup", (event) => {
+		if (event.code === 'Enter')
+		{
+			event.preventDefault();
+			document.getElementById("authButton").click();
+		}
+	});
+}
 
 const lobbyButtonClick = () => 
 {
     let lobbyName = byID('lobbyInput').value;
     let userName = formatUsername(byID('userInput').value);
 
-    setLocal('lobbyName', lobbyName);
-    setLocal('userName', userName);
+    document.cookie = 'lobbyName=' + lobbyName;
+    document.cookie = 'userName=' + userName;
 
-    if(!getLocal('userId')) 
+    if(getCookie('userId') === null) 
     {
-        addUserToList();
+        // addUserToList(byID('userInput').value)
+        // .then((response) => {return response.json()})
+        // .catch(e => {console.log(e)});
     }
 
-    redirect('components/lobby/lobby.html');
+    window.location = 'components/lobby/lobby.html';
 }
 
 /**
  * It might be better to move this into a function that strips characters from all input, since there are a couple other places where user input is taken
+ * (validateUsername, validateLobbyName, validateSomethingElse)
  * @param {*} username 
  * @returns 
  */
 const formatUsername = (username) => 
 {	
 	return username.replaceAll(/<|>/g, "").substring(0, 18);
-}
-
-const redirect = (path) => 
-{
-    let domain = document.location.href.split("http://")[1];
-    window.location.replace("http://" + getLocal("auth") + domain + path);
 }
 
 window.addEventListener('load', init);
