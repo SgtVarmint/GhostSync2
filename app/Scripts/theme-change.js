3bb6a7d8787cf106f4dbcf128221d53232261b7b
Joe Divinagracia <SgtVarmint2112@protonmail.com>
2023-02-10 00:15:12 -0500

    Removed themeChange from its own file since it was added to utils.js

--- app/Scripts/theme-change.js
+++
@@ -1,8 +0,0 @@
-function themeChanger() {
-    var root = document.querySelector(':root')
-
-    var rootStyles = getComputedStyle(root)
-    var mainColor = rootStyles.getPropertyValue('--primary')
-    console.log("main color = ", mainColor)
-    root.style.setProperty('--primary', '#3D315B')
-}
